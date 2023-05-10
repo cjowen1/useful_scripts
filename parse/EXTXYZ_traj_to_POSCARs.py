45581101ebfdf49b91fb47904ed4603770fb890d
Cameron J. Owen <85685582+cjowen1@users.noreply.github.com>
2023-05-10 14:14:38 -0400

    Update EXTXYZ_traj_to_POSCARs.py

--- parse/EXTXYZ_traj_to_POSCARs.py
+++ parse/EXTXYZ_traj_to_POSCARs.py
@@ -2,10 +2,8 @@ import numpy as np
 from ase.io import read, write
 import os, glob, shutil, sys, time
 
-n_frames = 0
-tic = time.time()
-dft_file = sys.argv[1]
-trj = read(dft_file, index=":")
-lmp_trj = []
-for i, frame in enumerate(trj):
-    write(f"frame_{i}.xyz",frame,format='vasp')
+trj_file = sys.argv[1] # read in trajectory file using system argument
+trj = read(trj_file, index=":") # parse individual frames into trj 
+
+for i, frame in enumerate(trj): # loop over frames and print POSCARs for use in VASP
+    write(f"POSCAR_{i}",frame,format='vasp')
