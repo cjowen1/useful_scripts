2b544a4e1cee886d185afb56a4171f2790c6d561
Cameron J. Owen <85685582+cjowen1@users.noreply.github.com>
2023-05-10 14:16:57 -0400

    Update OUTCAR_to_EXTXYZ.py

--- parse/OUTCAR_to_EXTXYZ.py
+++ parse/OUTCAR_to_EXTXYZ.py
@@ -4,31 +4,27 @@ import os
 from ase.io import write
 import shutil
 
-if not os.path.exists('recalc'):
+if not os.path.exists('recalc'): # create directory to collect new frames
     os.mkdir("recalc")
-
-    # Parse with ASE.
-        #write xyz file for each OUTCAR to concatenate later for offline training
         
-i = 0
-        
+i = 0   
 for n in range(1,318,1):
-    os.chdir(f"frame_{n}")
+    os.chdir(f"frame_{n}") # loops over directories labeled frame_{n} where independent vasp calculations were completed
     
-    traj = read_vasp_out("OUTCAR", index=slice(None))
+    traj = read_vasp_out("OUTCAR", index=slice(None)) # read VASP output, index = -1 might be necessary
 
-    answ = os.path.exists("frame_{n}.xyz")
+    answ = os.path.exists("frame_{n}.xyz") # try to write new file
     if(answ):
         pass
     else:
         write(f"frame_{n}.xyz",traj,format='extxyz')
 
-    ansf = os.path.exists("../recalc/frame_{n}.xyz")
+    ansf = os.path.exists("../recalc/frame_{n}.xyz") # move new file to collection-directory
     if(ansf):
         pass
     else:
         shutil.move(f"frame_{n}.xyz", "../recalc/")
 
     os.chdir("../")
-    print('successfully parsed frame', i) 
+    print('successfully parsed frame', i)  # job status
     i += 1
